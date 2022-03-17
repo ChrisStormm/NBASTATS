67b5beb8140e27cb8fe7dd9fbd56377f95596556
Chris Storm <cx.storm22@gmail.com>
2022-03-16 18:31:32 -0700

    StatGenerator outline

--- src/statModel/StatGenerator.java
+++ src/statModel/StatGenerator.java
@@ -1,5 +1,25 @@
 package statModel;
 
+import java.util.Map;
+
+//When given an NBA Players relevant stats is capable of generating Player Offensive Impact score(POIS),
+//Player Defense Impact Score(PDIS), and Player Impact Score (PIS)
 public class StatGenerator {
-    
+
+    public StatGenerator(Map<String, Double> playerStats){
+
+    }
+
+    public double playerImpactScore(){
+       throw new RuntimeException("method not implemented");
+    }
+
+    public double offensivePlayerImpactScore(){
+        throw new RuntimeException("method not implemented");
+    }
+
+    public double defensivePlayerImpactScore(){
+        throw new RuntimeException("method not implemented");
+    }
+
 }
