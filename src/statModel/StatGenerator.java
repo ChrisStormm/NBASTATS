f23ad48d28b35174f01af6fb0baff7c027fdcd1e
Chris Storm <cx.storm22@gmail.com>
2022-03-22 14:37:49 -0700

    Wrote defense player impact score algorithm

--- src/statModel/StatGenerator.java
+++ src/statModel/StatGenerator.java
@@ -1,13 +1,29 @@
 package statModel;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 //When given an NBA Players relevant stats is capable of generating Player Offensive Impact score(POIS),
 //Player Defense Impact Score(PDIS), and Player Impact Score (PIS)
 public class StatGenerator {
-
-    public StatGenerator(Map<String, Double> playerStats){
-
+    private final Map<String, Double> stats;
+    //List in format [points, Offensive rebounds, total rebounds, assists, fg%, ft attempt, 3PM made, 3PT percent, blocks, steals]
+    public StatGenerator(List<Double> playerStats){
+        stats = new HashMap<>();
+        if(playerStats.size() != 10){
+            throw new IllegalArgumentException("list size must be 10");
+        }
+        stats.put("Points", playerStats.get(0));
+        stats.put("OffensiveRebounds", playerStats.get(1));
+        stats.put("Rebounds", playerStats.get(2));
+        stats.put("Assists", playerStats.get(3));
+        stats.put("FG%", playerStats.get(4));
+        stats.put("FTA", playerStats.get(5));
+        stats.put("3PM", playerStats.get(6));
+        stats.put("3P%", playerStats.get(7));
+        stats.put("blocks", playerStats.get(8));
+        stats.put("steals", playerStats.get(9));
     }
 
     public double playerImpactScore(){
@@ -15,11 +31,18 @@ public class StatGenerator {
     }
 
     public double offensivePlayerImpactScore(){
-        throw new RuntimeException("method not implemented");
+        double result = 0.0;
+
+        return result;
     }
 
     public double defensivePlayerImpactScore(){
-        throw new RuntimeException("method not implemented");
+        double result = 0.0;
+        result += 3*stats.get("blocks");
+        result += 1.5*stats.get("steals");
+        //only defensive rebounds
+        result += 0.5*(stats.get("Rebounds") - stats.get("OffensiveRebounds"));
+        return result;
     }
 
 }
